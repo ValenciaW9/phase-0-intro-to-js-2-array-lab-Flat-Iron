4b029d0a0bbf1833d4acf92fffbc39e1e875aa9e
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-07 16:07:21 -0400

    phase-0-intro-to-js-2-array-lab-Flat-Iron/
    phase-0-intro-to-js-2-array-lab-Flat-Iron/

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-// Write your solution here
-beforeEach(function () {
-    CacheStorage.length = 0;
+const helpers = require('./helpers.js');
 
-    CacheStorage.push("Milo", "Otis", "Garfield");
+describe('index.js', function () {
+    describe('cats', function () {
+        it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+            expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+        });
+    });
 
+    beforeEach(function () {
+        cats.length = 0;
+        cats.push('Milo', 'Otis', 'Garfield');
+    });
+
+    describe('Array functions', function () {
+        beforeEach(function () {
+            cats.length = 0;
+            cats.push('Milo', 'Otis', 'Garfield');
+        });
+
+        describe('destructivelyAppendCat(name)', function () {
+            it('appends a cat to the end of the cats array', function () {
+                destructivelyAppendCat('Ralph');
+                expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
+            });
+        });
+
+        describe('destructivelyPrependCat(name)', function () {
+            it('prepends a cat to the beginning of the cats array', function () {
+                destructivelyPrependCat("Bob");
+                expect(cats).to.have.ordered.members(["Bob", "Milo", "Otis", "Garfield"]);
+            });
+        });
+
+        describe('destructivelyRemoveLastCat()', function () {
+            it('removes the last cat from the cats array', function () {
+                destructivelyRemoveLastCat();
+                expect(cats).to.have.ordered.members(["Milo", "Otis"]).and.to.not.include('Garfield');
+            });
+        });
+    });
 });
